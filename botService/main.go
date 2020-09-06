6be862c1a16b2629e659275127c844e6fe998623
David Ilenwabor <dave11ilen@gmail.com>
2020-09-06 05:15:22 +0100

    Impl server for word generation

--- botService/main.go
+++ botService/main.go
@@ -1,49 +1,13 @@
 package main
 
 import (
-	"LanguageBotService/grpcUtil"
-	"LanguageBotService/translator"
 	"LanguageBotService/wordgen"
-	"fmt"
-	"golang.org/x/net/context"
-	"google.golang.org/grpc"
-	"log"
-	"net"
 )
-const (
-	PORT       = "50051"
-	TcpAddress = "0.0.0.0:"
-)
-type server struct {
 
-}
-
-func (s server) TranslateNow(ctx context.Context, translate *grpcUtil.Translate) (*grpcUtil.Translate, error) {
-	return nil, nil
-}
 
 func main(){
-	fmt.Println("Hello go 1.15")
-	translator.Hi()
-
 	wordgen.Init()
-	if word, err := wordgen.GetRandomWord(7); err != nil{
-		fmt.Println("Error is ", err)
-	} else{
-		fmt.Println("Word is", word)
-	}
-
-	lis, err := net.Listen("tcp", TcpAddress + PORT)
-	if err != nil{
-		log.Fatal("Could not initialize listener due to ", err.Error())
-	}
-	fmt.Println("Running server on port: 50051")
-	s := grpc.NewServer()
-	grpcUtil.RegisterBotServiceServer(s, &server{})
-
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("Failed to start server %v", err)
-	}
+	Init()
 }
 
 
--- botService/service.go
+++ botService/service.go
@@ -1,1 +1,48 @@
 package main
+
+import (
+	"LanguageBotService/grpcUtil"
+	"LanguageBotService/wordgen"
+	"fmt"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+const (
+	PORT       = "50051"
+	TcpAddress = "0.0.0.0:"
+)
+
+func Init(){
+	lis, err := net.Listen("tcp", TcpAddress + PORT)
+	if err != nil{
+		log.Fatal("Could not initialize listener due to ", err.Error())
+	}
+	fmt.Println("Running server on port: 50051")
+	s := grpc.NewServer()
+	grpcUtil.RegisterBotServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to start server %v", err)
+	}
+}
+
+type server struct {
+
+}
+
+func (s server) GenerateWord(ctx context.Context, request *grpcUtil.WordRequest) (*grpcUtil.WordResponse, error) {
+	fmt.Println("Calling generate word...")
+	charCount := request.GetWordCount()
+	word,_ := wordgen.GetRandomWord(int(charCount))
+	return &grpcUtil.WordResponse{
+		Word:  word       ,
+		PartOfSpeech: "Part of speech is i dunno",
+		Definition:   []string{},
+		Example:      []string{},
+	}, nil
+}
+
+
